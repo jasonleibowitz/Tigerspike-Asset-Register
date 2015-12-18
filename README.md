506ae045949392edb69f989590ec94a7bef0f159
Jason Leibowitz <jasonaleibowitz@gmail.com>
2015-12-18 09:23:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# assetRegister
+
+a [Sails](http://sailsjs.org) application
---
+++ api/models/User.js
@@ -0,0 +1,30 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+      firstName: {
+          type: 'string'
+      },
+      lastName: {
+          type: 'string'
+      },
+      username: {
+          type: 'string',
+          required: true
+      },
+      password: {
+          type: 'string',
+          required: true
+      }
+      emailAddress: {
+          type: 'string'
+      },
+
+  }
+};
